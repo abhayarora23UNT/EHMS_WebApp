68a5fb3cb7b9b55dcd2af1c909cbb3a223c740a1
Meghana Gushidi <104174084+Meghana-G16@users.noreply.github.com>
2022-04-30 17:43:22 -0500

    code changes

--- Source Code/Front-End/VIEW/HTML/NURSE/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-unset($_SESSION["Nurse"]);
-unset($_SESSION["password"]);
-$error="logged-out successfuly";
-header("Location: nurse_login.php?error= $error");
-?>
--- Source Code/Front-End/VIEW/HTML/PAYMENT/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-unset($_SESSION["Finance_Email"]);
-unset($_SESSION["password"]);
-$error="logged-out successfuly";
-header("Location:finance_login.php?error= $error");
-?>
